15c251bfc58dd87cdeee38e75d097cf5b9d1cac0
WissemBT <wissembettaieb2000@gmail.com>
2022-02-26 14:55:45 +0100

    basicAppt

--- src/app/pages/appointement/appointement.module.ts
+++ src/app/pages/appointement/appointement.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AppointementRoutingModule } from './appointement-routing.module';
+import { FormsModule }   from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AppointementRoutingModule
+    AppointementRoutingModule,
+    FormsModule 
+
   ]
 })
 export class AppointementModule { }
